3bfca1a01e8a609d8d6035923fa5e16b9ed9ae94
ztizzlegaming <ztizzlegaming@gmail.com>
2015-07-20 17:33:11 -0400

    Began working on getting pot from database

--- src/php/default.php
+++ src/php/default.php
@@ -36,45 +36,6 @@ function getSteamProfileInfoForSteamID($allUsersInfoStr, $steamIDToFind) {
 	return false;
 }
 
-function generateChatArr() {
-	$db = getDB();
-	$stmt = $db->query('SELECT * FROM `chat` ORDER BY `id` DESC LIMIT 50');
-	$chatMessages = $stmt->fetchAll();
-
-	$allUserIDs = array();
-
-	foreach ($chatMessages as $message) {
-		$steamUserID = $message['steamUserID'];
-		array_push($allUserIDs, $steamUserID);
-	}
-
-	# Convert array to csv
-	$allUserIDsStr = join(',', $allUserIDs);
-
-	# Get all user info for the steam user IDs
-	$chatAPIKey = getSteamAPIKey('chat');
-	$usersInfoStr = file_get_contents("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=$chatAPIKey&steamids=$allUserIDsStr");
-
-	$chatMessagesArr = array();
-
-	for ($i1 = count($chatMessages) - 1; $i1 >= 0; $i1--) {
-		$message = $chatMessages{$i1};
-
-		$id = $message['id'];
-		$text = htmlspecialchars(stripcslashes($message['text']));
-		$date = $message['date'];
-		$time = $message['time'];
-
-		$steamUserID = $message['steamUserID'];
-		$steamUserInfo = getSteamProfileInfoForSteamID($usersInfoStr, $steamUserID);
-
-		$arr = array('id' => $id, 'text' => $text, 'date' => $date, 'time' => $time, 'steamUserInfo' => $steamUserInfo);
-		array_push($chatMessagesArr, $arr);
-	}
-
-	return $chatMessagesArr;
-}
-
 function jsonSuccess($data) {
 	return json_encode(array('success' => 1, 'data' => $data));
 }
@@ -97,6 +58,5 @@ function getSteamAPIKey($type) {
 		default:
 			return null;
 	}
-	;
 }
 ?>
--- src/php/update.php
+++ src/php/update.php
@@ -4,7 +4,56 @@ include 'default.php';
 $db = getDB();
 
 # Get all chat messages
-$chatMessagesArr = generateChatArr();
+$stmt = $db->query('SELECT * FROM `chat` ORDER BY `id` DESC LIMIT 50');
+$chatMessages = $stmt->fetchAll();
+
+$allUserIDs = array();
+
+foreach ($chatMessages as $message) {
+	$steamUserID = $message['steamUserID'];
+	array_push($allUserIDs, $steamUserID);
+}
+
+# Convert array to csv for steam API call
+$allUserIDsStr = join(',', $allUserIDs);
+
+# Get all user info for the steam user IDs
+$chatAPIKey = getSteamAPIKey('chat');
+$usersInfoStr = file_get_contents("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=$chatAPIKey&steamids=$allUserIDsStr");
+
+$chatMessagesArr = array();
+
+for ($i1 = count($chatMessages) - 1; $i1 >= 0; $i1--) {
+	$message = $chatMessages{$i1};
+
+	$id = $message['id'];
+	$text = htmlspecialchars(stripcslashes($message['text']));
+	$date = $message['date'];
+	$time = $message['time'];
+
+	$steamUserID = $message['steamUserID'];
+	$steamUserInfo = getSteamProfileInfoForSteamID($usersInfoStr, $steamUserID);
+
+	$arr = array('id' => $id, 'text' => $text, 'date' => $date, 'time' => $time, 'steamUserInfo' => $steamUserInfo);
+	array_push($chatMessagesArr, $arr);
+}
+
+# Get the current pot
+$stmt = $db->query('SELECT * FROM `currentPot`');
+
+$currentPotArr = $stmt->fetchAll();
+
+$currentPot = array();
+
+foreach ($currentPotArr as $itemInPot) {
+	$itemID = $itemInPot['id'];
+	$itemOwnerSteamID = $itemInPot['ownerSteamID'];
+	$itemName = $itemInPot['itemName'];
+	$itemPrice = $itemInPot['itemPrice'];
+
+	$arr = array('itemID' => $itemID, 'itemOwnerSteamID' => $itemOwnerSteamID, 'itemName' => $itemName, 'itemPrice' => $itemPrice);
+	array_push($currentPot, $arr);
+}
 
 $data = array('chat' => $chatMessagesArr);
 echo jsonSuccess($data);
