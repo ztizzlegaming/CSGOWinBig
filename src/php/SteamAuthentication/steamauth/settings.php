2d53ae5427eb91fefa86ad2e67f033d32050956e
ztizzlegaming <ztizzlegaming@gmail.com>
2015-07-21 14:04:01 -0400

    Moved steam API key outside of web root

--- src/php/SteamAuthentication/steamauth/settings.php
+++ src/php/SteamAuthentication/steamauth/settings.php
@@ -1,5 +1,16 @@
 <?php
-$steamauth['apikey'] = '1FBC1D48247E517DB7CE37C093450807'; // Your Steam WebAPI-Key found at http://steamcommunity.com/dev/apikey
+$fileLoc = $_SERVER['DOCUMENT_ROOT'] . '/../../passwords.txt';
+if (file_exists($fileLoc)) {
+	$fh = fopen($fileLoc, 'r');
+	$jsonStr = fgets($fh);
+	$arr = json_decode($jsonStr, true);
+	$apiKey = $arr['steamLoginAPIKey'];
+	fclose($fh);
+} else {
+	die('no file found');
+}
+
+$steamauth['apikey'] = $apiKey; // Your Steam WebAPI-Key found at http://steamcommunity.com/dev/apikey
 $steamauth['domainname'] = "http://csgowinbig.jordanturley.com/"; // The main URL of your website displayed in the login page
 $steamauth['buttonstyle'] = "large_no"; // Style of the login button [small|large_no|large]
 $steamauth['logoutpage'] = "../example.php"; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
